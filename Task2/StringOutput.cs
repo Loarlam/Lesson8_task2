528ed42cfdb769e233aece55063ec5c8b578c0ce
Loarlam <messiah02@mail.ru>
2020-02-26 18:53:54 +0300

    Создал статитческий класс StrngOutput. Добавил в него статический метод Pring без возвращаемого значения, но который позволяет выводить на экран строку в цвете. Создал структуру ColorType с перечислением парочки цветом.

---
+++ Task2/StringOutput.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Task2
+{
+    static class StringOutput
+    {
+        enum ColorType 
+        {
+            Red = 1,
+            Blue = 2,
+            Yellow = 3,
+            Green = 4
+        }
+
+        public static void Print(string stroka, int color)
+        {
+            switch (color)
+            {
+                case (int)ColorType.Red:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case (int)ColorType.Blue:
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    break;
+                case (int)ColorType.Yellow:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                case (int)ColorType.Green:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    break;
+                default:
+                    Console.WriteLine($"Цвета под цифрой {color} нет в нашей базе!");
+                    break;
+            }
+
+            Console.WriteLine(stroka);
+            Console.ResetColor();
+        }
+    }
+}
